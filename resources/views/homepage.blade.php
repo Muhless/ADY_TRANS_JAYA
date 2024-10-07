d6ec5c0c0a0e839aae8600a9a1ca09f832337b06
Muhless <nuryadi.muhta@gmail.com>
2024-10-07 22:44:46 +0700

    add tailwind

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Ady Trans Jaya CRUD</title>
+        @vite('resources/css/app.css')
+    </head>
+
+    <body>
+        <h1 class="font-extrabold text-red-600 text-9xl">
+            Hello Laravel
+        </h1>
+    </body>
+
+</html>
