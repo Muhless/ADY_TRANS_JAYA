e49bb7a38f85a1e360b92bafeebdaeb3103d8697
Muhless <nuryadi.muhta@gmail.com>
2024-10-16 22:51:03 +0700

    update

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,14 +5,30 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Ady Trans Jaya CRUD</title>
+        <title>Sistem Pendukung Keputusan</title>
         @vite('resources/css/app.css')
     </head>
 
-    <body>
-        <h1 class="font-extrabold text-red-600 text-9xl">
-            Hello Laravel
-        </h1>
+    <body class='p-5'>
+        <section class="navbar bg-blue-300">
+            <div class="flex justify-between">
+                <h1 class="font-extrabold text-5xl">
+                    Kelompok 1
+                </h1>
+                {{-- <div> --}}
+                <ul class="flex">
+                    <li class="mx-8"><a href='/'>Home</a></li>
+                    <li class="mx-8"><a href='/'>Keuangan</a></li>
+                    <li class="mx-8"><a href='/'>SPK</a></li>
+                    <li class="mx-8"><a href='/'>Kategori</a></li>
+                </ul>
+                {{-- </div> --}}
+            </div>
+
+            <p>Muhta Nuryadi</p>
+            <p>Ahmad Tri Prasetyo</p>
+            <p>Farizki Wisnuaji</p>
+        </section>
     </body>
 
 </html>
